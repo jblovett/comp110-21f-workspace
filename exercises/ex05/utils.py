d4467603e91fcc5aa3ba23f41dd31e3f24d8c167
jblovett <jblovett@live.unc.edu>
2021-10-07 16:38:54 -0400

    ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -3,3 +3,29 @@
 __author__ = "123456789"
 
 # Define your functions below
+
+
+def only_evens(input: list) -> list:
+    i = 0
+    output = list()
+    while i < len(input):
+        val = input[i]
+        if val % 2 == 0:
+            output.append(val)
+        i += 1
+    return output
+
+
+def sub(start: int, end: int, input: list) -> list:
+    output = list()
+    length = len(input)
+    if start < 0:
+        start = 0
+    if end > length:
+        end = length
+    if length == 0 or start > length or end <= 0:
+        return output
+    while start < end:
+        output.append(input[start])
+        start += 1
+    return output
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,16 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+
+from exercises.ex05.utils import only_evens, sub
 
 __author__ = "123456789"
+
+
+def test_all_evens():
+    all_evens: list = [2, 4, 6]
+    assert only_evens(all_evens) == [2, 4, 6]
+
+
+def test_sub():
+    short: list = [2, 5, 4, 3]
+    assert sub(1, 4, short) == [5, 4, 3]
